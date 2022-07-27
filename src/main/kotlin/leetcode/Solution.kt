44f9216059f3290a508e85fd9007db95cf33d5a6
ysnam <ysnam@blockchainglobal.co.kr>
2022-07-27 16:19:10 +0900

    Leet Code > SearchInsertPosition

--- src/main/kotlin/leetcode/Solution.kt
+++ src/main/kotlin/leetcode/Solution.kt
@@ -1,5 +1,7 @@
 package leetcode
 
+import leetcode.easy.MergeTwoSortedLists
+import leetcode.easy.SearchInsertPosition
 import leetcode.easy.TwoSum
 import leetcode.easy.ValidParentheses
 
@@ -9,8 +11,13 @@ class Solution {
 //        val res: IntArray? = TwoSum().twoSum(intArrayOf(2,7,11,15), 9)
 //        println("twoSum  ==>  ${res?.get(0)} ${res?.get(1)}")
 
-        // isValid
-        val res: Boolean = ValidParentheses().isValid("{}()[)")
-        println("😀😀 ============> ")
+//        // isValid
+//        val res: Boolean = ValidParentheses().isValid("{}()[)")
+
+//        // mergeTwoLists
+//        val res = MergeTwoSortedLists().mergeTwoLists()
+
+        val res = SearchInsertPosition().searchInsert(intArrayOf(2,7,11,15), 7)
+        println("😀😀 ============> $res")
     }
 }
